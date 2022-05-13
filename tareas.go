b065421219341585c10e77e5f084f2d059b97469
Fiorella <Fiorella@agropro.ag>
2022-05-13 09:50:59 -0300

    Se agregaron comentarios para mas legibilidad

--- tareas.go
+++ tareas.go
@@ -11,30 +11,40 @@ type listaDeTareas struct {
 	lista []*tarea
 }
 
+//Funcion para pasara a completa una tarea.
 func (t *tarea) pasarACompleta() {
 	t.completado = true
 }
 
+//Funcion para actualizar la descripcion de una tarea
 func (t *tarea) actualizarDescripcion(descripcion string) {
 	t.descripcion = descripcion
 }
 
+//Funcion para actualizar el nombre de una tarea
 func (t *tarea) actualizarNombre(nombre string) {
 	t.nombre = nombre
 }
 
+//Funcion para agregar una tarea a la lista
 func (l *listaDeTareas) agregarALista(tarea *tarea) {
 	l.lista = append(l.lista, tarea)
 }
+
+//Funcion para eliminar una tarea de la lista segun el numero.
 func (l *listaDeTareas) eliminarDeLista(numeroDeTarea int) {
 	l.lista = append(l.lista[:numeroDeTarea], l.lista[numeroDeTarea+1:]...)
 }
+
+//Funcion para imprimir toda la lista
 func (l *listaDeTareas) imprimirLista() {
 	for _, tarea := range l.lista {
 		fmt.Println("Nombre", tarea.nombre)
 		fmt.Println("Descripcion", tarea.descripcion)
 	}
 }
+
+//Funcion que imprime solo las tareas completas
 func (l *listaDeTareas) imprimirListaCompletados() {
 	for _, tarea := range l.lista {
 		if tarea.completado == true {
